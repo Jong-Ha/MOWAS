0c918aa5f511d9e52679f1cc01b363107e92a971
sunmi <cybersunmi91@gmail.com>
2022-10-06 20:49:14 +0900

    KSM 2022.10.06
    커뮤니티 신고 add, get, process
    단위테스트 완료, 지도 API class 추가

--- src/main/java/com/project/site/dao/SiteDaoImpl.java
+++ src/main/java/com/project/site/dao/SiteDaoImpl.java
@@ -1,7 +1,9 @@
 package com.project.site.dao;
 
 
+import com.project.common.Page;
 import com.project.common.Search;
+import com.project.domain.CommunityReport;
 import com.project.domain.MasterBoard;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository("siteDaoImpl")
 public class SiteDaoImpl implements SiteDao{
@@ -25,6 +28,7 @@ public class SiteDaoImpl implements SiteDao{
         // TODO Auto-generated constructor stub
     }
 
+    //공지사항 CRUD
     @Override
     public void addMasterBoard(MasterBoard masterBoard) {
 
@@ -46,12 +50,45 @@ public class SiteDaoImpl implements SiteDao{
         sqlSession.delete("SiteMapper.deleteMasterBoard", masterBoardNo);
     }
 
-
+    @Override
     public List<MasterBoard> listMasterBoard(Search search) throws Exception {
-        return sqlSession.selectList("SiteMapper.listMaterBoard", search);
+        return sqlSession.selectList("SiteMapper.listMasterBoard", search);
+    }
+    @Override
+    public int getMbTotalCount(Search search) throws Exception {
+        return sqlSession.selectOne("SiteMapper.getMbTotalCount", search);
+    }
+    //커뮤니티 신고 CRUD
+    @Override
+    public void addCommunityReport(CommunityReport communityReport) throws Exception {
+        sqlSession.insert("SiteMapper.addCommunityReport", communityReport);
+    }
+    @Override
+    public  CommunityReport getCommunityReport(int reportNo) throws Exception {
+        return sqlSession.selectOne("SiteMapper.getCommunityReport", reportNo);
+    }
+    @Override
+    public List<CommunityReport> listCommunityReport(int reportNo) throws Exception {
+        return sqlSession.selectList("SiteMapper.listCommunityReport");
+
+    }
+    @Override
+    public void processCommunityReport(CommunityReport communityReport) throws Exception {
+        sqlSession.update("SiteMapper.processCommunityReport", communityReport);
     }
 
-    public int getTotalCount(Search search) throws Exception {
-        return sqlSession.selectOne("SiteMapper.getTotalCount", search);
+    @Override
+    public  List<CommunityReport>listCommunityReportProcess(int reportNo) throws Exception {
+        return sqlSession.selectList("SiteMapper.listCommunityReportProcess");
+    }
+
+    @Override
+    public CommunityReport getCommunityReportProcess(int reportNo) throws Exception {
+        return sqlSession.selectOne("SiteMapper.getCommunityReportProcess", reportNo);
+    }
+
+    @Override
+    public int getTotalCount(Map<String,Object> map) throws Exception {
+        return sqlSession.selectOne("SiteMapper.getTotalCount", map);
     }
 }
