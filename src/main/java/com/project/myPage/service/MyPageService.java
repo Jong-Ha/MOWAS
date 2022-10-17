839640b6da4d84d872cbdf679d4e9fd3bed08720
Jang Ha Kyoung <apf2013@naver.com>
2022-10-17 10:33:45 +0900

    JHK 2022.10.17
    마이페이지 서비스 커밋

--- src/main/java/com/project/myPage/service/MyPageService.java
+++ src/main/java/com/project/myPage/service/MyPageService.java
@@ -1,4 +1,18 @@
 package com.project.myPage.service;
 
+import com.project.domain.User;
+
+import java.util.Map;
+
 public interface MyPageService {
+
+    public User getMyPage(String userId)throws Exception;
+
+    public Map<String, Object> getMyInfor(String userId)throws Exception;
+
+    public Map<String, Object> getMyBoard(String userId)throws Exception;
+
+
+
+
 }
